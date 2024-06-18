8c4e51a87a098db84813e0af529e7bf791706014
simon njoroge <mukirisimon22@gmail.com>
2024-06-18 13:16:35 +0300

    advice generator

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ const  fetchdata =()=>{
             const par=document.createElement("div")
             par.classList.add("adviced")
             par.innerHTML=`<p>${element.slipped.slip.advice}</p>`
+            const ima=document.createElement("div")
+            ima.classList.add("imaged")
+            ima.innerHTML=`<img src="./images/pattern-divider-desktop.svg" alt="">`
             console.log(par)
+            setTimeout(()=>{
             advicesomeone.append(par)
+            advicesomeone.append(ima)
+        },2000)
         });
     })
 }
